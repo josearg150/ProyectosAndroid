4b4132fb1e68c658ef6837a609dd57e98cdde1c9
Jose angel <73683594+josearg150@users.noreply.github.com>
2022-03-15 21:15:37 -0600

    avance u3imc

--- U3IMCApp/app/src/main/java/mx/edu/itl/c18131273/u3imcapp/MainActivity.java
+++ U3IMCApp/app/src/main/java/mx/edu/itl/c18131273/u3imcapp/MainActivity.java
@@ -1,3 +1,26 @@
+/*------------------------------------------------------------------------------------------
+:*                         TECNOLOGICO NACIONAL DE MEXICO
+:*                                CAMPUS LA LAGUNA
+:*                     INGENIERIA EN SISTEMAS COMPUTACIONALES
+:*                             DESARROLLO EN ANDROID "A"
+:*
+:*                   SEMESTRE: ENE-JUN/2022    HORA: 10-11 HRS
+:*
+:*                       Aplicacion para calcular el imc
+:*
+:*  Archivo     : MainActivity.java
+:*  Autor       : jose angel rocha garcia 18131273
+:*  Fecha       : 16/03/2022
+:*  Compilador  : Android Studio bumblebee 2021.1.1
+:*  Descripcion : Esta clase contiene los metodos para calcular y mostrar
+                    el resultado del calculo del imc usando el peso y la estatura
+                    ademas de mostrar la pantalla de acerca de
+:*  Ultima modif:
+:*  Fecha       Modifico             Motivo
+:*==========================================================================================
+:*
+:*------------------------------------------------------------------------------------------*/
+
 package mx.edu.itl.c18131273.u3imcapp;
 
 import androidx.appcompat.app.AlertDialog;
