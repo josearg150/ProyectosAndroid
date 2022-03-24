d4cb42b5f49dfb7aa0343d8043d6968892a49f2f
Jose angel <73683594+josearg150@users.noreply.github.com>
2022-03-23 21:17:38 -0600

    cambios minimos

--- U3IMCStringsApp/app/src/main/java/mx/edu/itl/c18131273/u3imcapp/SplashActivity.java
+++ U3IMCStringsApp/app/src/main/java/mx/edu/itl/c18131273/u3imcapp/SplashActivity.java
@@ -1,3 +1,24 @@
+/*------------------------------------------------------------------------------------------
+:*                         TECNOLOGICO NACIONAL DE MEXICO
+:*                                CAMPUS LA LAGUNA
+:*                     INGENIERIA EN SISTEMAS COMPUTACIONALES
+:*                             DESARROLLO EN ANDROID "A"
+:*
+:*                   SEMESTRE: ENE-JUN/2022    HORA: 10-11 HRS
+:*
+:*                       Aplicacion para calcular el imc
+:*
+:*  Archivo     : SplashActivity.java
+:*  Autor       : jose angel rocha garcia 18131273
+:*  Fecha       : 16/03/2022
+:*  Compilador  : Android Studio bumblebee 2021.1.1
+:*  Descripcion : Esta clase muestra una pantalla de carga e invoca a la pantalla
+                    de la clase MainActivity
+:*  Ultima modif:
+:*  Fecha       Modifico             Motivo
+:*==========================================================================================
+:* 18/03/2022  Jose angel rocha garcia   Agregar idioma ingles
+:*------------------------------------------------------------------------------------------*/
 package mx.edu.itl.c18131273.u3imcapp;
 
 import androidx.appcompat.app.AppCompatActivity;
